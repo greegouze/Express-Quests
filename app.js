ffec28149999fb1f66ea4ac974782a7960aae771
Greg <104724644+greegouze@users.noreply.github.com>
2023-10-15 16:20:30 +0200

    quest 2.2

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 3000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -14,6 +15,10 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+//route user
+app.get("/api/users", movieHandlers.getUser);
+app.get("/api/users/:id", movieHandlers.getUserId);
 
 app.listen(port, (err) => {
   if (err) {
